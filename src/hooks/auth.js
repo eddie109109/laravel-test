9b65947d4fc13914c362cdf5680ac6afde93a3d2
jundi_chen <cjd_eddie@hotmail.com>
2023-05-07 16:30:46 -0700

    commit

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -11,6 +11,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             .get('/api/user')
             .then(res => res.data)
             .catch(error => {
+                console.log('failed to get the user', error)
                 if (error.response.status !== 409) throw error
 
                 router.push('/verify-email')
